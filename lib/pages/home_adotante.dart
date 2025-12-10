2fb280afba5d5bfd717f819bf63f2817fdc5fb27
Laís Brandão <134562545+lalalandlindaaa@users.noreply.github.com>
2025-12-10 08:51:02 -0400

    Create HomeAdotante widget for adopter panel

---
+++ lib/pages/home_adotante.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeAdotante extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Painel do Adotante")),
+    );
+  }
+}
+
